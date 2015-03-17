5cd7507e9040133768f05839e360f629872f1901
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-16 18:57:36 -0700

    animated the projectile

--- THProjectileNode.m
+++ THProjectileNode.m
@@ -15,9 +15,21 @@
     THProjectileNode *projectile = [self spriteNodeWithImageNamed:@"projectile_1"];
     projectile.position = position;
     projectile.name = @"Projectile";
+    [projectile setupAnimation];
     
     return projectile;
     
+}
+
+- (void) setupAnimation {
+    
+    NSArray *textures = @[[SKTexture textureWithImageNamed:@"projectile_1"],
+                          [SKTexture textureWithImageNamed:@"projectile_2"],
+                          [SKTexture textureWithImageNamed:@"projectile_3"]];
+    SKAction *animation = [SKAction animateWithTextures:textures timePerFrame:0.1];
+    
+    SKAction *repeatAction = [SKAction repeatActionForever:animation];
+    [self runAction:repeatAction];
 }
 
 @end
